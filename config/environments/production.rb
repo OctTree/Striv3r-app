010c08c5cf046d4f19d6946dd7e5c520d80dac53
kishore-124 <kishorekce124@gmail.com>
2021-12-18 21:57:24 +0530

    fixed changes

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -108,7 +108,7 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.default_url_options = { host: ENV['STRIV3_DOMAIN'] }
   config.action_mailer.smtp_settings = {
-    domain: ENV['STRIV3_DOMAIN'],
+    domain: "yourdomain.com",
     address: 'smtp.sendgrid.net',
     port: 587,
     authentication: :plain,
