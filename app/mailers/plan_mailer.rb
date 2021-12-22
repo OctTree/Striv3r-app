f7c6e6909f1166ca2cc2efc583a435b1305405de
kishore-124 <kishorekce124@gmail.com>
2021-12-22 22:54:44 +0530

    changes

--- app/mailers/plan_mailer.rb
+++ app/mailers/plan_mailer.rb
@@ -9,7 +9,7 @@ class PlanMailer < ApplicationMailer
   def send_plan_email_to_admin(plan_id)
     @plan = Plan.find(plan_id)
 
-    mail(to: "gopalakrishnanv708@gmail.com", subject: 'Striv3 new plan')
+    mail(to: "graham@striv3r.com", subject: 'Striv3 new plan')
   end
 
   def send_customized_plan_email(user_id)
