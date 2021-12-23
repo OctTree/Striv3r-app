c4b8847c480472b043f328e04fded6d3663be573
kishore-124 <kishorekce124@gmail.com>
2021-12-23 14:00:50 +0530

    fixed issue

--- app/controllers/api/v1/activity_plans_controller.rb
+++ app/controllers/api/v1/activity_plans_controller.rb
@@ -2,6 +2,6 @@ class Api::V1::ActivityPlansController < ApplicationController
   before_action :authorize_request
 
   def index
-    @activity_plans = current_user.activity_plans.where("activity_at >= ? and activity_at <= ? and status = ?", Date.current.beginning_of_month, Date.current.end_of_month, "approved").all
+    @activity_plans = current_user.activity_plans.where("activity_at >= ? and activity_at <= ? and status = ?", Date.current.beginning_of_month, Date.current.end_of_month, 1).all
   end
 end
