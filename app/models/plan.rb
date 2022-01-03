4d38e18287dac784adf84117ebabafc0bb917667
kishore-124 <kishorekce124@gmail.com>
2022-01-03 22:51:56 +0530

    fixed issues

--- app/models/plan.rb
+++ app/models/plan.rb
@@ -26,7 +26,13 @@ class Plan < ApplicationRecord
 
   def create_activity_plan
     1.upto(4) do |week|
+
       1.upto(days_per_week) do
+        ActivityPlan.create(user_id: user_id, activity_name: "meditate", week: "week #{week}",
+                            time: minutes, frequency: frequency_days, activity_at: Date.current, frequency_days: days_per_week, frequency_minutes: minutes)
+      end
+
+      1.upto(frequency_days) do
         activity_type.each do |activity_type|
           ActivityPlan.create(user_id: user_id, activity_name: activity_type, week: "week #{week}",
                               time: minutes, frequency: frequency_days, activity_at: Date.current, frequency_days: frequency_days, frequency_minutes: frequency_minutes)
