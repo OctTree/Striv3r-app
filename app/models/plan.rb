e5aac2e2918d1128b613079f3d34059c0e54df92
kishore-124 <kishorekce124@gmail.com>
2022-01-05 14:35:27 +0530

    fixed changes

--- app/models/plan.rb
+++ app/models/plan.rb
@@ -25,14 +25,18 @@ class Plan < ApplicationRecord
   end
 
   def create_activity_plan
+
     1.upto(3) do |week|
+      week_date = Date.current  if week == 1
+      week_date = (Date.current + 7.days)  if week == 2
+      week_date = (Date.current + 14.days) if week == 3
 
       ActivityPlan.create(user_id: user_id, activity_name: "meditate", week: "week #{week}",
-                          time: minutes, frequency: days_per_week, activity_at: Date.current, frequency_days: days_per_week, frequency_minutes: minutes)
+                          time: minutes, frequency: days_per_week, activity_at: week_date, frequency_days: days_per_week, frequency_minutes: minutes)
 
       activity_type.each do |activity_type|
         ActivityPlan.create(user_id: user_id, activity_name: activity_type, week: "week #{week}",
-                            time: minutes, frequency: frequency_days, activity_at: Date.current, frequency_days: frequency_days, frequency_minutes: frequency_minutes)
+                            time: minutes, frequency: frequency_days, activity_at: week_date, frequency_days: frequency_days, frequency_minutes: frequency_minutes)
       end
 
     end
