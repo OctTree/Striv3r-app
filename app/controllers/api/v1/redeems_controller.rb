3195d484bc2d36d3c98493867dc3011fde906959
kishore-124 <kishorekce124@gmail.com>
2022-01-08 09:36:24 +0530

    fixed changes

--- app/controllers/api/v1/redeems_controller.rb
+++ app/controllers/api/v1/redeems_controller.rb
@@ -1,9 +1,10 @@
 class Api::V1::RedeemsController < ApplicationController
+  before_action :authorize_request
 
   def create
     point_redeem = current_user.point_redeems.new(redeems_params)
     if point_redeem.save
-      current_user.update(point_balance: 0, total_earned_point: 0)
+      current_user.update(point_balance: 0, total_point_earned: 0)
       success_json_response({message: "Created successfully."})
     else
       error_json_response point_redeem.errors.full_messages, :bad_request
