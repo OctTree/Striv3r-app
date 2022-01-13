4d588bd6d242b6a1f8bc83cd61c62dc290304034
kishore-124 <kishorekce124@gmail.com>
2022-01-13 23:24:05 +0530

    add column for user index

--- app/controllers/api/v1/redeems_controller.rb
+++ app/controllers/api/v1/redeems_controller.rb
@@ -2,18 +2,6 @@ class Api::V1::RedeemsController < ApplicationController
   before_action :authorize_request
 
   def create
-    point_redeem = current_user.point_redeems.new(redeems_params)
-    if point_redeem.save
-      current_user.update(point_balance: 0, total_point_earned: 0)
-      success_json_response({message: "Created successfully."})
-    else
-      error_json_response point_redeem.errors.full_messages, :bad_request
-    end
-  end
-
-  private
-
-  def redeems_params
-    params.require(:point_redeem).permit(:redeemed_point)
+    UserMailer.send_redeem_email(current_user.id)
   end
 end
--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -3,4 +3,10 @@ class UserMailer < ApplicationMailer
     @user = User.find(id)
     mail(to: "graham@striv3r.com", subject: 'Account deactivation')
   end
+
+  def send_redeem_email(id)
+    @user = User.find(id)
+
+    mail(to: "graham@striv3r.com", subject: 'Redeem request')
+  end
 end
