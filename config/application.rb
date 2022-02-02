b87a5348acf6fa6f4eb2bb2ed89f1e511604e17c
kishore-124 <kishorekce124@gmail.com>
2022-02-02 10:54:38 +0530

    fixed cors

--- config/application.rb
+++ config/application.rb
@@ -29,7 +29,7 @@ module Striv3r
 
     #CORS CONFIG
     config.middleware.insert_before 0, Rack::Cors do
-      ALLOWED_CLIENTS = %i[localhost:3000 localhost:5000 striv3.herokuapp.com striv3r-prod-backend.herokuapp.com] + [ENV['CORS_CLIENTS']&.split(',')]
+      ALLOWED_CLIENTS = %i[localhost:3000 localhost:5000 striv3.herokuapp.com striv3r-prod-backend.herokuapp.com striv3r-backend.herokuapp.com] + [ENV['CORS_CLIENTS']&.split(',')]
 
       ALLOWED_CLIENTS.compact.each do |client|
         allow do
